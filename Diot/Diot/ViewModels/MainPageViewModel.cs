5ec61a24c0c8b5a7f611ab7cb4f513de6d4fb721
Mike <mikelarsenis@gmail.com>
2019-09-30 20:55:09 -0600

    Remove tabbed pages, remove settings, introduce base page.

--- Diot/Diot/ViewModels/MainPageViewModel.cs
+++ Diot/Diot/ViewModels/MainPageViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Diot.Helpers;
 using Diot.Interface;
+using Diot.Models;
+using Prism.Navigation;
 using Prism.Services;
 using Xamarin.Forms;
 
@@ -9,12 +13,27 @@ namespace Diot.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        #region Fields
+
+        private List<MovieDbModel> _moviesList = new List<MovieDbModel>();
+
+        #endregion
+
         #region Properties
 
         /// <summary>
         ///     Gets the navigate to add new page command.
         /// </summary>
         public ICommand NavigateToAddNewPageCommand => new Command(async () => { await navigateToAddNewPage(); });
+
+        /// <summary>
+        ///     Gets or sets the movies list.
+        /// </summary>
+        public List<MovieDbModel> MoviesList
+        {
+            get => _moviesList;
+            set => SetProperty(ref _moviesList, value);
+        }
 
         #endregion
 
@@ -39,6 +58,37 @@ namespace Diot.ViewModels
         private async Task navigateToAddNewPage()
         {
             await NavigationService.NavigateAsync(PageNames.AddNewPage);
+        }
+
+        /// <summary>
+        ///     Gets the cover images.
+        /// </summary>
+        private async Task getCoverImagesAsync()
+        {
+            var updatedList = new List<MovieDbModel>();
+
+            foreach (var movie in MoviesList)
+            {
+                var imgSource = await MoviesDbHelper.GetMovieCover(movie.Poster_Path, 200);
+                movie.CoverImage = imgSource == null ? "" : ImageSource.FromStream(() => new MemoryStream(imgSource));
+                updatedList.Add(movie);
+            }
+
+            MoviesList = updatedList;
+        }
+
+        /// <summary>
+        ///     Called when [navigating to].
+        /// </summary>
+        public override void OnNavigatingTo(INavigationParameters parameters)
+        {
+            base.OnNavigatingTo(parameters);
+            MoviesList = DbService.GetAllMovies();
+
+            Task.Run(async () =>
+            {
+                await getCoverImagesAsync();
+            });
         }
 
         #endregion
