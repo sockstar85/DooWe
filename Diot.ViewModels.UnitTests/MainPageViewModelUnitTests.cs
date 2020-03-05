36cac68da6f9d2d0bb41a36722f0a35aefec727e
Mike <mikelarsenis@gmail.com>
2020-03-04 22:50:57 -0700

    Add unit tests to the main page vm.

--- Diot.ViewModels.UnitTests/MainPageViewModelUnitTests.cs
+++ Diot.ViewModels.UnitTests/MainPageViewModelUnitTests.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Diot.Helpers;
 using Diot.Interface;
 using Diot.Interface.Manager;
+using Diot.Models;
 using Moq;
+using Newtonsoft.Json;
+using Prism.Navigation;
 using Prism.Services;
 using Xunit;
 
@@ -14,7 +18,23 @@ namespace Diot.ViewModels.UnitTests
 		#region Fields
 
 		public static IEnumerable<object[]> ConstructorArguments = getConstructorArguments();
-		
+
+		private List<MovieDbModel> _moviesList = new List<MovieDbModel>
+		{
+			new MovieDbModel
+			{
+				Title = "Avengers : Endgame"
+			},
+			new MovieDbModel
+			{
+				Title = "Avengers : Age of Ultron"
+			},
+			new MovieDbModel
+			{
+				Title = "The Iron Giant"
+			}
+		};
+
 		#endregion
 
 		#region Helper Methods
@@ -28,6 +48,24 @@ namespace Diot.ViewModels.UnitTests
 			Mock<IHttpClientService> httpClientService = null,
 			Mock<IConnectivityManager> connectivityManager = null)
 		{
+
+			if (connectivityManager == null)
+			{
+				connectivityManager = new Mock<IConnectivityManager>();
+				connectivityManager.Setup(x => x.HasNetworkConnection).Returns(true);
+			}
+
+			if (navigationService == null)
+			{
+				navigationService = new Mock<IExtendedNavigation>();
+				navigationService.Setup(x => x.NavigateAsync(It.IsAny<string>(), It.IsAny<INavigationParameters>(), It.IsAny<bool>(), It.IsAny<bool>()))
+					.ReturnsAsync(
+					new NavigationResult
+					{
+						Success = true
+					});
+			}
+
 			return new MainPageViewModel(
 				navigationService?.Object ?? new Mock<IExtendedNavigation>().Object,
 				pageDialogService?.Object ?? new Mock<IPageDialogService>().Object,
@@ -134,15 +172,92 @@ namespace Diot.ViewModels.UnitTests
 		{
 			#region Arrange
 
-			var viewModel = createViewModel();
+			var navigationService = new Mock<IExtendedNavigation>();
+			navigationService = new Mock<IExtendedNavigation>();
+			navigationService.Setup(x => x.NavigateAsync(It.IsAny<string>(), It.IsAny<INavigationParameters>(), It.IsAny<bool>(), It.IsAny<bool>()))
+				.ReturnsAsync(
+				new NavigationResult
+				{
+					Success = true
+				});
+
+			var viewModel = createViewModel(navigationService);
 
 			#endregion
 
 			#region Act
 
+			viewModel.NavigateToAddNewPageCommand.Execute(null);
+
 			#endregion
 
 			#region Assert
+
+			navigationService.Verify(x => x.NavigateAsync(PageNames.AddNewPage, It.IsAny<INavigationParameters>(), It.IsAny<bool>(), It.IsAny<bool>()), Times.Once);
+
+			#endregion
+		}
+
+		[Fact]
+		public void NavigateToMovieDetailsCommand_NavigatesToNewPage()
+		{
+			#region Arrange
+
+			var navigationService = new Mock<IExtendedNavigation>();
+			navigationService = new Mock<IExtendedNavigation>();
+			navigationService.Setup(x => x.NavigateAsync(It.IsAny<string>(), It.IsAny<INavigationParameters>(), It.IsAny<bool>(), It.IsAny<bool>()))
+				.ReturnsAsync(
+				new NavigationResult
+				{
+					Success = true
+				});
+
+			var viewModel = createViewModel(navigationService);
+
+			#endregion
+
+			#region Act
+
+			viewModel.NavigateToMovieDetailsCommand.Execute(new MovieDbModel());
+
+			#endregion
+
+			#region Assert
+
+			navigationService.Verify(x => x.NavigateAsync(PageNames.MovieDetailsPage, It.IsAny<INavigationParameters>(), It.IsAny<bool>(), It.IsAny<bool>()), Times.Once);
+
+			#endregion
+		}
+
+		[Fact]
+		public void SearchCommand_ReturnsCorrectValues()
+		{
+			#region Arrange
+
+			var viewModel = createViewModel();
+
+			viewModel.MoviesList = _moviesList;
+
+			var searchCriteria = "Avengers";
+			var expectedValues = new List<MovieDbModel>
+			{
+				_moviesList[0],
+				_moviesList[1]
+			};
+
+			#endregion
+
+			#region Act
+
+			viewModel.TitleSearch = searchCriteria;
+
+			#endregion
+
+			#region Assert
+
+			Assert.NotNull(viewModel.SortedMoviesList);
+			Assert.Equal(expectedValues.Count, viewModel.SortedMoviesList.Count);
+			Assert.Equal(expectedValues, viewModel.SortedMoviesList);
 
 			#endregion
 		}
